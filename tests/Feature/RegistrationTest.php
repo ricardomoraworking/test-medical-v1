e2a8a551451fbc48df61d62258b916bd17488f4e
ricardomoraworking <ricardo.mora.working@gmail.com>
2023-05-22 19:30:35 -0500

    adding test

---
+++ tests/Feature/RegistrationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_registration_screen_can_be_rendered(): void
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_new_users_can_register(): void
+    {
+        $response = $this->post('/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'phone' => '3504284724',
+            'comment' => 'this is a comment',
+        ]);
+
+        $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+}
